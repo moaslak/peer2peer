c6bcaba83df8e3b2134f822fe843b4d825d658b7
moaslak <moaslak@gmail.com>
2022-04-26 12:39:45 +0200

    project start

---
+++ peer2peer/peer2peer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace p2p
+{
+    public class Program
+    {
+        static string PeerAddress = "";
+        static int PeerPort = 31337;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.ReadLine();
+            ReceiveNumber();
+        }
+
+        public static int ReceiveNumber()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Parse("0.0.0.0"), PeerPort);
+            Console.WriteLine($"Listening on port {PeerPort}");
+
+            listener.Start();
+
+            Console.WriteLine("Awaiting connection...");
+            
+            TcpClient tcpClient = listener.AcceptTcpClient();
+            Console.WriteLine("Connection established");
+            Console.ReadLine();
+
+            return 0;
+        }
+    }
+}
